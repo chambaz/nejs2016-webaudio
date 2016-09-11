52936c400d529f2ea988354014adc46921d4b60a
Adam Chambers <adammchambers@gmail.com>
2016-09-11 15:10:50 -0400

    Include Reveal JS libs/plugins

--- slides/app.js
+++ slides/app.js
@@ -1,4 +1,8 @@
 const Reveal = require('reveal.js');
 
-Reveal.initialize();
-console.log(Reveal);
+Reveal.initialize({
+    dependencies: [
+        // Syntax highlight for <code> elements
+        { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+    ]
+});
