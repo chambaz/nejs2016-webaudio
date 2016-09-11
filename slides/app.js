ecd191ae68597765af2febcd9b47a0a6f675f6b8
Adam Chambers <adammchambers@gmail.com>
2016-09-11 15:55:14 -0400

    Code styles, browser support, node list

--- slides/app.js
+++ slides/app.js
@@ -1,6 +1,7 @@
 const Reveal = require('reveal.js');
 
 Reveal.initialize({
+	history: true,
     dependencies: [
         // Syntax highlight for <code> elements
         { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
