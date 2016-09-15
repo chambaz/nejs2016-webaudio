04c33af4ab8f0acf12d9bf0cbd1fbbcd12f5cec0
Adam Chambers <adammchambers@gmail.com>
2016-09-14 20:32:32 -0400

    Add pulsating pitney bowes logo

--- slides/app.js
+++ slides/app.js
@@ -7,3 +7,44 @@ Reveal.initialize({
         { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
     ]
 });
+
+// create web audio context
+const audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+
+// create analyzer node
+const analyser = audioCtx.createAnalyser();
+
+// set size of fast fourier transform
+analyser.fftSize = 32;
+
+// construct Uint8Array for analysis data
+const bufferLength = analyser.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
+
+const logo = document.querySelector('[data-logo]');
+
+// get user microphone
+navigator.mediaDevices.getUserMedia({
+	audio: true,
+}).then(stream => {
+	// create media stream from mic input and connect to analyzer
+	const mediaStream = audioCtx.createMediaStreamSource(stream);
+	mediaStream.connect(analyser);
+
+	// kick off analysis
+	requestAnimationFrame(analyze);
+});
+
+// call analyze function at 60fps
+function analyze() {
+	// get frequency data of audio at this point in time
+	analyser.getByteFrequencyData(dataArray);
+	const scale = dataArray[10] / 50;
+
+	if (scale > 1) {
+		logo.style.transform = `scale(${scale})`;
+	}
+
+	// recursivly call function every 60fps
+	requestAnimationFrame(analyze);
+}
